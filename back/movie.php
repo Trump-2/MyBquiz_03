b66204602079d2e8eaefb867871cbe1a8e6ae9ca
YIHONG <akashidaiki2@gmail.com>
2024-01-12 10:34:44 +0800

    建立編輯院線片畫面

--- back/movie.php
+++ back/movie.php
@@ -1,32 +1,32 @@
 <button onclick="location.href='?do=add_movie'">新增電影</button>
 <hr>
 <style>
-  .item div {
-    box-sizing: border-box;
-    color: black;
-  }
+.item div {
+  box-sizing: border-box;
+  color: black;
+}
 
-  .item {
-    background-color: white;
-    width: 100%;
-    display: flex;
-    padding: 3px;
-    box-sizing: border-box;
-    margin: 3px 0;
-  }
+.item {
+  background-color: white;
+  width: 100%;
+  display: flex;
+  padding: 3px;
+  box-sizing: border-box;
+  margin: 3px 0;
+}
 
-  /* 這三個構成 100 %，分為 7 等分 */
-  .item>div:nth-child(1) {
-    width: 15%
-  }
+/* 這三個構成 100 %，分為 7 等分 */
+.item>div:nth-child(1) {
+  width: 15%
+}
 
-  .item>div:nth-child(2) {
-    width: 12%
-  }
+.item>div:nth-child(2) {
+  width: 12%
+}
 
-  .item>div:nth-child(3) {
-    width: 73%
-  }
+.item>div:nth-child(3) {
+  width: 73%
+}
 </style>
 
 <div style="width:100%; overflow:auto;height:415px;">
@@ -36,39 +36,41 @@
 
   ?>
 
-    <div class="item ">
-      <div>
-        <img src="./img/<?= $movie['poster'] ?>" style="width:100%" alt="">
-      </div>
-      <div>
-        分級:<img src="./icon/03C0<?= $movie['level'] ?>.png" alt="" style="width:25px">
+  <div class="item ">
+    <div>
+      <img src="./img/<?= $movie['poster'] ?>" style="width:100%" alt="">
+    </div>
+    <div>
+      分級:<img src="./icon/03C0<?= $movie['level'] ?>.png" alt="" style="width:25px">
 
+    </div>
+    <div>
+      <div style="display:flex; width:100%">
+        <div style="width:33.33%">
+          片名:<?= $movie['name'] ?>
+        </div>
+        <div style="width:33.33%">
+          片長:<?= $movie['length'] ?>
+        </div>
+        <div style="width:33.33%">
+          上映日期:<?= $movie['ondate'] ?>
+        </div>
       </div>
       <div>
-        <div style="display:flex; width:100%">
-          <div style="width:33.33%">
-            片名:<?= $movie['name'] ?>
-          </div>
-          <div style="width:33.33%">
-            片長:<?= $movie['length'] ?>
-          </div>
-          <div style="width:33.33%">
-            上映日期:<?= $movie['ondate'] ?>
-          </div>
-        </div>
-        <div>
-          <button class="show-btn" data-id="<?= $movie['id'] ?>">
-            <?= ($movie['sh'] == 1) ? '顯示' : '隱藏' ?></button>
-          <button class="sw-btn" data-id="<?= $movie['id'] ?>" data-sw="<?= ($idx !== 0) ? $movies[$idx - 1]['id'] : $movie['id'] ?>">往上</button>
-          <button class="sw-btn" data-id="<?= $movie['id'] ?>" data-sw="<?= ((count($movies) - 1) != $idx) ? $movies[$idx + 1]['id'] : $movie['id'] ?>">往下</button>
-          <button class="edit-btn" data-id="<?= $movie['id'] ?>">編輯電影</button>
-          <button class="del-btn" data-id="<?= $movie['id'] ?>">刪除電影</button>
-        </div>
-        <div>
-          劇情介紹:<?= $movie['intro'] ?>
-        </div>
+        <button class="show-btn" data-id="<?= $movie['id'] ?>">
+          <?= ($movie['sh'] == 1) ? '顯示' : '隱藏' ?></button>
+        <button class="sw-btn" data-id="<?= $movie['id'] ?>"
+          data-sw="<?= ($idx !== 0) ? $movies[$idx - 1]['id'] : $movie['id'] ?>">往上</button>
+        <button class="sw-btn" data-id="<?= $movie['id'] ?>"
+          data-sw="<?= ((count($movies) - 1) != $idx) ? $movies[$idx + 1]['id'] : $movie['id'] ?>">往下</button>
+        <button class="edit-btn" data-id="<?= $movie['id'] ?>">編輯電影</button>
+        <button class="del-btn" data-id="<?= $movie['id'] ?>">刪除電影</button>
+      </div>
+      <div>
+        劇情介紹:<?= $movie['intro'] ?>
       </div>
     </div>
+  </div>
 
   <?php
   }
@@ -76,48 +78,49 @@
   ?>
 </div>
 <script>
-  $(".show-btn").on("click", function() {
-    let id = $(this).data('id');
-    $.post("./api/show.php", {
-      id
-    }, () => {
-      // 方法一，跟 server 重新要整份資料
-      location.reload();
+$(".show-btn").on("click", function() {
+  let id = $(this).data('id');
+  $.post("./api/show.php", {
+    id
+  }, () => {
+    // 方法一，跟 server 重新要整份資料
+    location.reload();
 
-      // 方法二
-      // switch ($(this).text()) {
-      //   case "隱藏":
-      //     $(this).text("顯示");
-      //     break;
-      //   case "顯示":
-      //     $(this).text("隱藏");
-      //     break;
-      // }
-    });
-  })
-  $(".sw-btn").on('click', function() {
-    let id = $(this).data('id');
-    let sw = $(this).data('sw');
+    // 方法二
+    // switch ($(this).text()) {
+    //   case "隱藏":
+    //     $(this).text("顯示");
+    //     break;
+    //   case "顯示":
+    //     $(this).text("隱藏");
+    //     break;
+    // }
+  });
+})
+$(".sw-btn").on('click', function() {
+  let id = $(this).data('id');
+  let sw = $(this).data('sw');
 
-    let table = 'movie'
-    $.post("./api/sw.php", {
-      id,
-      sw,
-      table
-    }, () => {
-      location.reload();
-    })
+  let table = 'movie'
+  $.post("./api/sw.php", {
+    id,
+    sw,
+    table
+  }, () => {
+    location.reload();
   })
-  $(".edit-btn").on('click', function() {
-
+})
+$(".edit-btn").on('click', function() {
+  let id = $(this).data('id');
+  location.href = `?do=edit_movie&id=${id}`;
+})
+$(".del-btn").on('click', function() {
+  let id = $(this).data('id');
+  $.post("./api/del.php", {
+    id,
+    table: 'movie'
+  }, () => {
+    location.reload();
   })
-  $(".del-btn").on('click', function() {
-    let id = $(this).data('id');
-    $.post("./api/del.php", {
-      id,
-      table: 'movie'
-    }, () => {
-      location.reload();
-    })
-  })
+})
 </script>
